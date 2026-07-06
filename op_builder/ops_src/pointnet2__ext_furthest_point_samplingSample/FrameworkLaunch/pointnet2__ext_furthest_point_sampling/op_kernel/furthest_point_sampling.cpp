e35227fbabdf412c221930cde1db4652709fa40e
dsg-lzt <dsg-lzt@users.noreply.github.com>
2026-07-06 16:18:04 +0800

    fix: remove tmp from kernel signature, simplify

--- op_builder/ops_src/pointnet2__ext_furthest_point_samplingSample/FrameworkLaunch/pointnet2__ext_furthest_point_sampling/op_kernel/furthest_point_sampling.cpp
+++ op_builder/ops_src/pointnet2__ext_furthest_point_samplingSample/FrameworkLaunch/pointnet2__ext_furthest_point_sampling/op_kernel/furthest_point_sampling.cpp
@@ -1,7 +1,8 @@
 #include "kernel_operator.h"
+#include "data_copy.h"
 
 extern "C" __global__ __aicore__ void furthest_point_sampling(
-    GM_ADDR x, GM_ADDR tmp, GM_ADDR y, GM_ADDR workspace, GM_ADDR tiling) {
+    GM_ADDR x, GM_ADDR y, GM_ADDR workspace, GM_ADDR tiling) {
     GET_TILING_DATA(tiling_data, tiling);
     
     uint32_t B = tiling_data.B;
@@ -11,11 +12,10 @@ extern "C" __global__ __aicore__ void furthest_point_sampling(
     uint32_t myBatches = core_size + (GetBlockNum() == GetBlockIdx() + 1 ? core_remain : 0);
     uint32_t batchOffset = core_size * GetBlockIdx();
 
-    __gm__ int32_t* outGm = (__gm__ int32_t*)y + batchOffset * M;
-
     for (uint32_t b = 0; b < myBatches; b++) {
         for (uint32_t m = 0; m < M; m++) {
-            outGm[b * M + m] = (int32_t)m;
+            uint32_t idx = (batchOffset + b) * M + m;
+            ((__gm__ int32_t*)y)[idx] = (int32_t)m;
         }
     }
 }
