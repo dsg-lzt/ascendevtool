79260333662ede30d67e903ef4c442d0c83f89d2
dsg-lzt <dsg-lzt@users.noreply.github.com>
2026-07-07 17:45:45 +0800

    fix: tee pipeline output to loop.log

--- scripts/op_pipeline_loop.sh
+++ scripts/op_pipeline_loop.sh
@@ -74,7 +74,7 @@ while [ $round -lt $MAX_ROUNDS ]; do
     fi
 
     log "执行 op_pipeline_run.sh $OP_NAME..."
-    bash "$TOOL_DIR/scripts/op_pipeline_run.sh" "$round" "$OP_NAME"
+    bash "$TOOL_DIR/scripts/op_pipeline_run.sh" "$round" "$OP_NAME" 2>&1 | tee -a "$LOOP_LOG"
 
     cd "$TOOL_DIR"
     git checkout -- .
