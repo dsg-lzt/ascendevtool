1bb0496d7e06ed591e0b7eda054fe69a2c126587
dsg-lzt <dsg-lzt@users.noreply.github.com>
2026-07-07 14:29:00 +0800

    debug: minimal kernel writing [0,1,2...] to isolate framework vs algorithm

--- op_builder/ops_src/FurthestPointSamplingSample/FrameworkLaunch/FurthestPointSampling/op_kernel/furthest_point_sampling.cpp
+++ op_builder/ops_src/FurthestPointSamplingSample/FrameworkLaunch/FurthestPointSampling/op_kernel/furthest_point_sampling.cpp
@@ -1,8 +1,5 @@
 #include "kernel_operator.h"
 
-constexpr int32_t BUF_NUM = 2;
-constexpr int32_t COORD_DIM = 3;
-
 template<typename T>
 class KernelFPS {
 public:
@@ -11,11 +8,7 @@ public:
     __aicore__ inline void Init(GM_ADDR p, GM_ADDR s,
                                 uint32_t B, uint32_t N, uint32_t M,
                                 uint32_t tileN, uint32_t bpc, uint32_t crem, float iv) {
-        B_ = B; N_ = N; M_ = M; tileN_ = tileN; initVal_ = (T)iv;
-        if (tileN_ > N_) tileN_ = N_;
-        if (tileN_ < 64) tileN_ = 64;
-
-        inGm = reinterpret_cast<__gm__ T*>(p);
+        B_ = B; N_ = N; M_ = M;
         outGm = reinterpret_cast<__gm__ int32_t*>(s);
 
         uint32_t bi = AscendC::GetBlockIdx();
@@ -23,72 +16,18 @@ public:
         else { start_ = crem * (bpc + 1) + (bi - crem) * bpc; end_ = start_ + bpc; }
         if (start_ >= B_) end_ = start_;
         if (end_ > B_) end_ = B_;
-
-        pipe.InitBuffer(qX, tileN_ * sizeof(T));
-        pipe.InitBuffer(md, N_ * sizeof(T));
     }
 
     __aicore__ inline void Process() {
-        for (uint32_t b = start_; b < end_; b++) ProcessBatch(b);
-    }
-
-private:
-    __aicore__ inline void ProcessBatch(uint32_t b) {
-        __gm__ T* batchIn = inGm + b * N_ * COORD_DIM;
-        __gm__ int32_t* batchOut = outGm + b * M_;
-
-        AscendC::LocalTensor<T> mdAll = md.Get<T>();
-        for (uint32_t i = 0; i < N_; i++) mdAll.SetValue(i, initVal_);
-
-        T selX = batchIn[0], selY = batchIn[1], selZ = batchIn[2];
-        batchOut[0] = 0;
-
-        int32_t nTiles = (N_ + tileN_ - 1) / tileN_;
-        for (int32_t m = 1; m < (int32_t)M_; m++) {
-            float gMax = -65504.0f;
-            uint32_t gIdx = 0;
-
-            for (int32_t t = 0; t < nTiles; t++) {
-                int32_t ts = t * tileN_;
-                int32_t cN = (ts + (int32_t)tileN_ <= (int32_t)N_) ? tileN_ : (N_ - ts);
-                if (cN <= 0) continue;
-
-                AscendC::LocalTensor<T> xTile = qX.Get<T>();
-                for (int32_t i = 0; i < cN; i++) {
-                    int32_t gi = ts + i;
-                    xTile.SetValue(i, batchIn[gi * COORD_DIM + 0]);
-                }
-
-                float lM = -65504.0f;
-                uint32_t lI = 0;
-                for (int32_t i = 0; i < cN; i++) {
-                    int32_t gi = ts + i;
-                    float dx = (float)xTile.GetValue(i) - (float)selX;
-                    float dy = (float)batchIn[gi * COORD_DIM + 1] - (float)selY;
-                    float dz = (float)batchIn[gi * COORD_DIM + 2] - (float)selZ;
-                    float nd = dx * dx + dy * dy + dz * dz;
-                    float od = (float)mdAll.GetValue(ts + i);
-                    if (nd < od) mdAll.SetValue(ts + i, (T)nd);
-                    float cd = (float)mdAll.GetValue(ts + i);
-                    if (cd > lM) { lM = cd; lI = i; }
-                }
-                if (lM > gMax) { gMax = lM; gIdx = ts + lI; }
-            }
-
-            batchOut[m] = (int32_t)gIdx;
-            int32_t off = gIdx * COORD_DIM;
-            selX = batchIn[off + 0]; selY = batchIn[off + 1]; selZ = batchIn[off + 2];
-            mdAll.SetValue(gIdx, (T)0.0f);
+        for (uint32_t b = start_; b < end_; b++) {
+            __gm__ int32_t* batchOut = outGm + b * M_;
+            for (uint32_t i = 0; i < M_; i++) batchOut[i] = (int32_t)i;
         }
     }
 
-    AscendC::TPipe pipe;
-    AscendC::TBuf<AscendC::QuePosition::VECCALC> qX;
-    AscendC::TBuf<AscendC::QuePosition::VECCALC> md;
-    __gm__ T* inGm;
+private:
     __gm__ int32_t* outGm;
-    uint32_t B_, N_, M_, tileN_, start_, end_;
-    T initVal_;
+    uint32_t B_, N_, M_, start_, end_;
 };
 
 extern "C" __global__ __aicore__ void furthest_point_sampling(
